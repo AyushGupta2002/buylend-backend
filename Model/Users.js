ce34b107f4f2ae56e4fa5750a953b8ff67432b1f
Kartik1627 <95166123+Kartik1627@users.noreply.github.com>
2022-05-17 20:36:54 +0530

    Update Users.js

--- Model/Users.js
+++ Model/Users.js
@@ -2,50 +2,40 @@
 
 const { pool } = require("../pool_connection");
 const bcrypt = require("bcrypt");
-// const { encodeToken } = require("../src/config/njwt");
-// const createUser = await pool.query(
-//   `INSERT INTO buylend_schema.users values(1, raam,email@email.com,password,phone_number,address)`,
-//   res.json("Added!");
-const Query1 = `INSERT INTO buylend_schema.users values('145', 'kartik', 'hwhwe@gmail.com', 'pass', '827382', 'hsjajdsa kj09')`;
-pool.query(Query1, (error, results) => {
-  if (error) console.log(error);
-});
+const { encodeToken } = require("../src/config/njwt");
+const addUser = async (name, email, password, phone_number, address) => {
+  // finding whether email is already registered
+   console.log(1)
+  const existingUser =0 
+  await pool.query(
+    `SELECT * FROM buylend_schema.users where email = ?`,
+    [email],(err,result)=>{
 
-// const addUser = async (name, email, password, phone_number, address) => {
-//   // finding whether email is already registered
-//   const existingUser = await pool.query(
-//     `SELECT id FROM buylend_schema.users where phone_number = '${phone_number}'`
-//   );
-//   if (existingUser) return { error: "Phone Number is already taken" };
+      if(err)   throw(err)
+      if(result.length!=0)  existingUser=1;   
+    }
+  );
+  console.log(name)
+  if (existingUser) return { error: "User already exists" };
 
-//   password = await bcrypt.hash(password, 8);
+  password = await bcrypt.hash(password, 8);
+    const id=100;
+  const createUser = await pool.query(
+    `INSERT INTO buylend_schema.users ('id','name','email','password','phone_number','address') values (?,?,?,?,?,?)`,
+    [id, name, email, password, phone_number, address]
+  );
+  //console.log("hello");
+     return { name, email, password, phone_number, address };
+};
 
-//   const createUser = await pool.query(
-//     "INSERT INTO buylend_schema.users values(1, raam,email@email.com,password,phone_number,address)",
-//     [id, name, email, password, phone_number, address]
-//   );
+const verifyUser = async (email, password) => {
+  const existingUser = pool.query(
+    "select password from users where email === email"
+  );
+  if (!existingUser) return { error: `${email} is not registered!` };
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (validPassword) {
+  }
+};
 
-//   console.log("hello");
-//   //   return { user };
-// };
-
-// const verifyUser = async (email, password) => {
-//   const existingUser = pool.query(
-//     "select password from users where email === email"
-//   );
-//   if (!existingUser) return { error: `${email} is not registered!` };
-//   const validPassword = await bcrypt.compare(password, user.password);
-//   if (validPassword) {
-//   }
-// };
-
-// addUser(
-//   "1",
-//   "hello",
-//   "hello@hello",
-//   "123456789",
-//   "1234567890",
-//   "hello8 989898"
-// );
-// console.log("added");
-// module.exports = { addUser, verifyUser };
+ module.exports = { addUser, verifyUser };
