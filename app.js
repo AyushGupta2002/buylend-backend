ce3e68ca99c9a853bdf41670be75a829d0d26ddd
Kartik1627 <95166123+Kartik1627@users.noreply.github.com>
2022-05-18 03:23:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,73 +3,53 @@ const express = require("express");
 const app = express();
 const cookie = require("cookie-parser");
 const { pool } = require("./pool_connection");
-// const { BASE_URL, USER, ADDRESS, EDUCATION, RELATIVE, BANK, FINANCE, SERVICE, PINCODE, HEALTH, MAIN } = require("./src/utilities/routes");
-// const userPaths = require("./src/controllers/user");
-// const AddressPaths = require("./src/controllers/address");
-// const EducationPaths = require("./src/controllers/education");
-// const FamilyPaths = require("./src/controllers/family_details");
-// const BankPaths = require("./src/controllers/bank_details");
-// const FinancePaths = require("./src/controllers/financial_details");
-// const ServicePaths = require("./src/controllers/service_access");
-// const PincodePaths = require("./src/controllers/pincode_details");
-// const HealthPaths = require("./src/controllers/health_history");
-// const MainPaths = require("./src/controllers/main");
-// const { getUrl } = require("./src/utilities/helpers");
-const auth = require("./auth");
 const Pool = require("pg").Pool;
+const auth = require("./auth");
+
+const { BASE_URL } = require("./src/utilities/routes");
+
+const Users = require("./src/models/users");
+const Approval = require("./src/models/approvals");
+const Organisations = require("./src/models/organisations");
+const Products = require("./src/models/products");
+const UserOrgs = require("./src/models/user_orgs");
+const ProductModeDetails = require("./src/models/product_mode_details");
+const ChatRoom = require("./src/models/chat_room");
+const Messages = require("./src/models/messages");
+// const ChatServer = require("./src/config/chat");
+
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cookie());
-// common handling of requests
-// app.use(BASE_URL, (req, res, next) => {
-//   console.info(req.method, req.url);
-//   next();
-// });
-app.use(bodyParser.urlencoded({ extended: false }));
 
-const cors = require("cors");
+ // common handling of requests
+app.use(BASE_URL, (req, res, next) => {
+  console.info(req.method, req.url);
+  next();
+});
+const cors = require('cors');
 const corsOptions = {
   origin(origin, callback) {
     callback(null, true);
   },
   credentials: true,
-  exposedHeaders: "Content-disposition, x-suggested-filename",
+  exposedHeaders: 'Content-disposition, x-suggested-filename',
 };
 app.use(cors(corsOptions));
 
-// Map Model paths with controllers
-// app.use(getUrl(USER), userPaths);
-// app.use(getUrl(ADDRESS), AddressPaths);
-// app.use(getUrl(EDUCATION), EducationPaths);
-// app.use(getUrl(RELATIVE), FamilyPaths);
-// app.use(getUrl(FINANCE), FinancePaths);
-// app.use(getUrl(SERVICE), ServicePaths);
-// app.use(getUrl(PINCODE), PincodePaths);
-// app.use(getUrl(HEALTH), HealthPaths);
-// app.use(getUrl(MAIN), MainPaths);
-// app.use(getUrl(BANK), BankPaths);
-
-// app.get("/", (req, res) => {
-//   const Query = `SELECT * from buylend_schema.organisations`;
-//   pool.query(
-//     Query,
-//     (error, result) => {
-//       res.json({ response: result.rows });
-//     }
-//   );
-// });
-
-// app.use((req, res) => {
-//   res.status(404);
-//   res.send({
-//     error: { message: "Sorry, this is an invalid URL" },
-//   });
-// });
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome" });
-// });
+// creating models if not created
+// Users.schemaFuncs();
+// Approval.schemaFuncs();
+// Organisations.schemaFuncs();
+// Products.schemaFuncs();
+// UserOrgs.schemaFuncs();
+// ProductModeDetails.schemaFuncs();
+// ChatRoom.schemaFuncs();
+// Messages.schemaFuncs();
 
 app.use("/", auth);
+app.use('/',require('./src/controllers/chatApi'));
 
-app.listen(3000, console.log("App is now ready on localhost:3000"));
+console.log("App is now ready on localhost:3000");
+app.listen(3000);
